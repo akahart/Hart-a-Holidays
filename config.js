dca29dcd515dd5f50233f42dd8c40b47a7ba0e20
akahart <aka.hart@outlook.com>
2026-02-07 22:41:02 +1100

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 // Replace these values with your Firebase project credentials
 
 export const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: AIzaSyDO__hszC3tpVFWRdIfIp1EqKUmWefug,
+    authDomain: hart-s-holidays.firebaseapp.com,
+    projectId: hart-s-holidays,
+    storageBucket: hart-s-holidays.firebasestorage.app,
+    messagingSenderId: 187887555901,
+    appId: 1:187887555901:web:2ac4217a1affc109a1e154
 };
 
 export const ADMIN_PASSWORD = "Hilife69!";
